965a9982121d53c35f98b917dd9a5d419f039518
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-01-11 18:17:21 -0300

    Configure cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import subjects from './routes/subjects.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/subjects', subjects);
 
